c8ea432e7b71c2981873f17f9c18f60f990769a7
ManuMartinez94 <manumony1@gmail.com>
2024-07-06 17:08:22 +0200

    Add more dependencies

--- KotlinExercise/build.gradle.kts
+++ KotlinExercise/build.gradle.kts
@@ -8,12 +8,6 @@ plugins {
 group = "com"
 version = "0.0.1-SNAPSHOT"
 
-/*java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(17)
-	}
-}*/
-
 repositories {
 	mavenCentral()
 }
@@ -21,6 +15,11 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.24")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
